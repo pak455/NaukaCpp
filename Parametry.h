d70889692dc4ffd79623b2da504f5b8ed677a9d5
alekkal <aleksandrakaleta96@interia.eu>
2016-10-19 22:31:32 +0200

    Wszystkie pliki programowe

---
+++ Parametry.h
@@ -0,0 +1,9 @@
+//#pragma once
+#include <string>
+
+#ifndef _PARAMETRY_H
+#define	_PARAMETRY_H
+
+bool parse_parameters(int argc, char**argv, std::string& input_file_name, std::string& input_file_name_2, std::string& output_file_name);
+
+#endif
---
+++ Pomoc.h
@@ -0,0 +1,8 @@
+//#pragma once
+
+#ifndef _POMOC_H
+#define _POMOC_H
+
+void help();
+
+#endif
